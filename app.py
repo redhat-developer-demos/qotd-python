1090d03db8647142ef385f423e1e842e9252d22e
Don Schenck <dschenck@redhat.com>
2025-08-19 12:57:07 -0400

    Added Swagger

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import flask
 from flask import request, jsonify, make_response
+from flask_swagger_ui import get_swaggerui_blueprint
 from flask_cors import CORS
 import random
 import socket
@@ -8,6 +9,18 @@ import json
 app = flask.Flask(__name__)
 CORS(app)
 app.config["DEBUG"] = True
+
+SWAGGER_URL = '/swagger'
+API_URL =  '/static/swagger.json'
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "qotd-python"
+    }
+)
+
+app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
 
 quotes = [
     {'id': 0,
